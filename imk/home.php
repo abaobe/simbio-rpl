2cff528b211871437fd68c6c06970054bd29e2ea
nobody <nobody@dc7aad73-a9a1-3e2a-a4e3-45dd7bddeea7>
2010-11-30 22:54:34 +0000

    Home sudah kaya mokap

--- imk/home.php
+++ imk/home.php
@@ -1,19 +1,52 @@
 <?php
-
 // Set judul halaman yang bakal tampil di titlebar browser
 $judul_halaman = "Home";
 
 // Ambil header...
 include 'header.php';
-
 ?>
+
+<style type="text/css">
+
+    table.scorer {
+        border-collapse: collapse;
+    }
+
+    .scorer td, .scorer th {
+        border: 1px solid #DDD;
+        padding: 5px 10px;
+    }
+
+    .scorer th {
+        background: #CCCCCC url("images/nav-bg00.png") top center no-repeat;
+        text-shadow: 0px 1px 1px #DDD;
+    }
+
+    .scorer .gold {
+        background-color: #FFF42F;
+    }
+
+    .scorer .silver {
+        background-color: #E8E8E8;
+    }
+    
+    .scorer .bronze {
+        background-color: #FFF;
+    }
+
+    .scorer .me {
+        font-weight: bold;
+        color: #FF6600;
+    }
+
+</style>
 
 <div id="maincontent">
     <div id="content">
 
         <div class="postwrap fix">
 
-            <div class="post-36 post type-post hentry category-uncategorized" id="post-36">
+            <div class="post type-post hentry category-uncategorized" id="post-36">
 
                 <div class="copy fix">
 
@@ -21,7 +54,7 @@ include 'header.php';
                     <div class="post-header fix post-nothumb">
                         <div class="post-title-section fix">
                             <div class="post-title fix">
-                                <h2>Judul Halaman</h2>
+                                <h2>Selamat datang</h2>
                             </div>
                         </div>
                     </div>
@@ -33,44 +66,22 @@ include 'header.php';
 
                     <div class="textcontent">
                         <p>
-                            Kerjakan segala sesuatunya di sini.
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
+                            Sistem ini digunakan untuk
+                            pembelajaran pemrograman C secara online
+	   sebagai penunjang
+	   bagi peserta matakuliah Algoritme dan Pemrograman.
+                        </p><p>
+	   Sistem ini terdiri dari beberapa chapter, yang akan diberikan secara
+                            bertahap.
+	   Setiap mahasiswa dapat mengikuti pembelajaran pada suatu chapter
+                            jika sudah menyelesaikan semua
+	   tugas yang diberikan pada chapter sebelumnya (<strong>nilai setiap
+                                latihan masing-masing adalah 100</strong>)
                         </p>
+                        <hr/>
+                        <p style="font-size: 20px">Total nilai Anda: 2700</p>
+                        <p>Chapter terakhir: chapter 1 <br/>
+                           Latihan terakhir: <a href="#">pjj0213</a></p>
                     </div>
                 </div>
 
@@ -79,6 +90,75 @@ include 'header.php';
 
             </div>
 
+        </div>
+
+        <br/>
+
+        <div class="postwrap fix">
+
+            <div class="post type-post hentry category-uncategorized" id="post-36">
+                <div class="copy fix">
+                    <div class="post-header fix post-nothumb">
+                        <div class="post-title-section fix">
+                            <div class="post-title fix">
+                                <h2>Top 30 scorer</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="copy fix">
+                    <div class="textcontent">
+                        <table width="98%" class="scorer">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>NRP</th>
+                                    <th>Kelas</th>
+                                    <th>Nilai</th>
+                                    <th>Medali</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="gold"><td>1</td><td>G64090013</td><td>7</td><td>3080</td><td>Gold</td></tr>
+                                <tr class="gold"><td>2</td><td>G64090023</td><td>2</td><td>3000</td><td>Gold</td></tr>
+                                <tr class="gold"><td>3</td><td>G64090001</td><td>1</td><td>3000</td><td>Gold</td></tr>
+                                <tr class="gold"><td>4</td><td>G64090031</td><td>4</td><td>2900</td><td>Gold</td></tr>
+                                <tr class="gold"><td>5</td><td>G64090011</td><td>6</td><td>2800</td><td>Gold</td></tr>
+                                <tr class="silver"><td>6</td><td>G64090010</td><td>7</td><td>2750</td><td>Silver</td></tr>
+                                <tr class="silver me"><td>7</td><td>G64080000</td><td>7</td><td>2700</td><td>Silver</td></tr>
+                                <tr class="silver"><td>8</td><td>G64090076</td><td>1</td><td>2600</td><td>Silver</td></tr>
+                                <tr class="silver"><td>9</td><td>G64090055</td><td>4</td><td>2590</td><td>Silver</td></tr>
+                                <tr class="silver"><td>10</td><td>G64090051</td><td>7</td><td>2560</td><td>Silver</td></tr>
+                                <tr class="silver"><td>11</td><td>G54090002</td><td>3</td><td>2500</td><td>Silver</td></tr>
+                                <tr class="silver"><td>12</td><td>G54090010</td><td>7</td><td>2480</td><td>Silver</td></tr>
+                                <tr class="silver"><td>13</td><td>G14090011</td><td>5</td><td>2440</td><td>Silver</td></tr>
+                                <tr class="silver"><td>14</td><td>G64090098</td><td>1</td><td>2410</td><td>Silver</td></tr>
+                                <tr class="silver"><td>15</td><td>G64090022</td><td>6</td><td>2400</td><td>Silver</td></tr>
+                                <tr class="bronze"><td>16</td><td>G14090020</td><td>1</td><td>2390</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>17</td><td>G14090077</td><td>7</td><td>2370</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>18</td><td>G64090057</td><td>8</td><td>2350</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>19</td><td>G64090033</td><td>1</td><td>2300</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>20</td><td>G54090047</td><td>5</td><td>2280</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>21</td><td>G64090088</td><td>8</td><td>2200</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>22</td><td>G74090032</td><td>3</td><td>2100</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>23</td><td>G64090008</td><td>6</td><td>2100</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>24</td><td>F14090009</td><td>5</td><td>2080</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>25</td><td>G64090044</td><td>8</td><td>2050</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>26</td><td>G54090017</td><td>6</td><td>2000</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>27</td><td>G64090090</td><td>7</td><td>1900</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>28</td><td>F14090020</td><td>5</td><td>1890</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>29</td><td>G64090015</td><td>5</td><td>1800</td><td>Bronze</td></tr>
+                                <tr class="bronze"><td>30</td><td>G54090037</td><td>7</td><td>1750</td><td>Bronze</td></tr>
+                            </tbody>
+                        </table>
+
+                        <br/>
+
+
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div class="clear"></div>
